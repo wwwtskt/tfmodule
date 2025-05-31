879257479c8c44d73be556c3cc6f41abed6f0336
wwwtskt <th.iws.soo@gmail.com>
2025-05-31 23:59:18 +0900

    first commit

--- aws/container/public_ecr/variables.tf
+++ aws/container/public_ecr/variables.tf
@@ -1,4 +1,5 @@
 variable "repository_name" {
+  description = "repository name"
   type = string
 }
 
@@ -10,19 +11,23 @@ variable "prefix" {
 variable "about_text" {
   description = "text"
   type = string
+  default = "default"
 }
 
 variable "description" {
   description = "description"
   type = string
+  default = "default"
 }
 
 variable "logo_image_blob" {
   description = "log image blob"
   type = string
+  default = "default"
 }
 
 variable "usage_text" {
   description = "usage text"
   type = string
+  default = "default"
 }
